a7eba6920679723db248037591f13106c313ed82
Douglas Heriot <douglas@douglasheriot.com>
2012-12-25 18:30:26 +1100

    Initial Commit

---
+++ QCStageDisplay/QCStageDisplayPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  QCStageDisplayPlugIn.h
+//  QCStageDisplay
+//
+//  Created by Douglas Heriot on 25/12/12.
+//  Copyright (c) 2012 Douglas Heriot. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface QCStageDisplayPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
